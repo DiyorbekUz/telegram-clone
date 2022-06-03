298aa3db772c70e72b772164a6a7888a2218d6e5
DiyorbekUz <diyorbekermamatov@gmail.com>
2022-06-04 00:38:00 +0500

    finish App

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,11 +5,12 @@ import MessageModel from '../models/message.js'
 
 const sequelize = new Sequelize({
     dialect: 'postgres',
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
+    // username: process.env.PG_USER,
+    // password: process.env.PG_PASSWORD,
+    // host: process.env.PG_HOST,
+    // port: process.env.PG_PORT,
+    // database: process.env.PG_DATABASE,
+    connectionString: "postgres://benjneuyaiyfiw:b02e354a2d2e8b2742c6eb4a360b22640a0bed311e8f9250461fd268be5aeb71@ec2-54-147-33-38.compute-1.amazonaws.com:5432/d5t3joes7bl5id",
     logging: false
 })
 
