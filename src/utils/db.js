55d615e5c04a9e9bbf45bedcde572414ba973f75
DiyorbekUz <diyorbekermamatov@gmail.com>
2022-06-04 02:28:53 +0500

    finish app

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,17 +2,20 @@ import { Sequelize } from 'sequelize'
 
 import UserModel from '../models/user.js'
 import MessageModel from '../models/message.js'
-
-const sequelize = new Sequelize({
-    dialect: 'postgres',
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    // connectionString: "postgres://benjneuyaiyfiw:b02e354a2d2e8b2742c6eb4a360b22640a0bed311e8f9250461fd268be5aeb71@ec2-54-147-33-38.compute-1.amazonaws.com:5432/d5t3joes7bl5id",
-    logging: false
-})
+if (process.env.NODE_ENV === "production") {
+    const sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+    const sequelize = new Sequelize({
+        dialect: 'postgres',
+        username: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT,
+        database: process.env.PG_DATABASE,
+        // connectionString: "postgres://benjneuyaiyfiw:b02e354a2d2e8b2742c6eb4a360b22640a0bed311e8f9250461fd268be5aeb71@ec2-54-147-33-38.compute-1.amazonaws.com:5432/d5t3joes7bl5id",
+        logging: false
+    })
+}
 
 export default async function () {
     try {
