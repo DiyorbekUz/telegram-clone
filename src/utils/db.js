564505e068f926ede5f553b70194dafeae0708e4
DiyorbekUz <diyorbekermamatov@gmail.com>
2022-06-04 03:04:48 +0500

    finish app

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,25 +3,14 @@ import { Sequelize } from 'sequelize'
 import UserModel from '../models/user.js'
 import MessageModel from '../models/message.js'
 
-// let devConfig = {
-//     dialect: 'postgres',
-//     username: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     logging: false
-// }
-
-let devConfig = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
-
-let proConfig = {
-    connectionString: process.env.DATABASE_URL,
-} 
-
 const sequelize = new Sequelize({
     dialect: 'postgres',
-    connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    logging: false
 })
 
 export default async function () {
